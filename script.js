d1ba96911a999b61f637b566d2d91a114f5535b7
Ryan Soeder <mr.soeder@gmail.com>
2020-08-10 18:39:07 -0400

    deleted console.log

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-// PQxsFLRrnOeYjpsUPqnkYBqjA7uGGteU
 
 const peopleDiv = document.querySelector("#people");
 const mapDiv = document.querySelector("#map");
 const spacePeopleUrl = "http://api.open-notify.org/astros.json";
 
 function peopleHTML(json) {
-  console.log(json);
   let heading = `<h1>There are ${json.number} people in SPACE</h1>
-                <p>They are:</p>`;
+                 <p>They are:</p>`;
   let list = "";
   json.people.map((person) => {
     list += `<ul>
@@ -33,15 +31,15 @@ function getMap() {
         json.iss_position.latitude +
         "," +
         json.iss_position.longitude +
-        "&size=500,400&zoom=3&banner=location+of+our+heros";
+        "&size=500,400&zoom=4&banner=location+of+our+heros";
       mapDiv.innerHTML = `<img src=${map}>`;
     });
   setTimeout(getMap, 10000);
 }
 
 fetch(spacePeopleUrl)
-  .then((response) => response.json())
+  .then(response => response.json())
   .then(peopleHTML)
-  .catch((err) => console.log(err));
+  .catch(err => console.log(err));
 
 getMap();
