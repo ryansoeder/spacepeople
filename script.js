2fe301a95693d10ca5d54635b645c3413cf81157
Ryan Soeder <mr.soeder@gmail.com>
2020-08-10 16:01:57 -0400

    wrapped up iss location + mapquest

--- script.js
+++ script.js
@@ -1,52 +1,47 @@
+// PQxsFLRrnOeYjpsUPqnkYBqjA7uGGteU
 
 const peopleDiv = document.querySelector("#people");
-const address = document.querySelector("#address");
-const btn = document.querySelector("button");
+const mapDiv = document.querySelector("#map");
 const spacePeopleUrl = "http://api.open-notify.org/astros.json";
-const mapQuestUrl = "http://open.mapquestapi.com/geocoding/v1/address?key=PQxsFLRrnOeYjpsUPqnkYBqjA7uGGteU&location=";
 
-function peopleHTML (json) {
-    console.log(json);
-    let heading = `<h1>There are ${json.number} people in SPACE</h1>
+function peopleHTML(json) {
+  console.log(json);
+  let heading = `<h1>There are ${json.number} people in SPACE</h1>
                 <p>They are:</p>`;
-    let list = "";
-    json.people.map( (person) => {
-        list += `<ul>
+  let list = "";
+  json.people.map((person) => {
+    list += `<ul>
                 <li>Name: ${person.name}</li>
                 <li>Aboard: ${person.craft}</li>
                 </ul>`;
+  });
+
+  let html = heading + list;
+  peopleDiv.innerHTML = html;
+}
+
+function getMap() {
+  const issUrl = "http://api.open-notify.org/iss-now.json";
+  const mapQuestUrl =
+    "https://open.mapquestapi.com/staticmap/v5/map?key=PQxsFLRrnOeYjpsUPqnkYBqjA7uGGteU&center=";
+
+  fetch(issUrl)
+    .then((response) => response.json())
+    .then((json) => {
+      let map =
+        mapQuestUrl +
+        json.iss_position.latitude +
+        "," +
+        json.iss_position.longitude +
+        "&size=500,400&zoom=3&banner=location+of+our+heros";
+      mapDiv.innerHTML = `<img src=${map}>`;
     });
-
-    let html = heading + list;
-    peopleDiv.innerHTML = html;
+  setTimeout(getMap, 10000);
 }
-
-function latLng (json) {
-    let latitude = json.results[0].locations[0].displayLatLng.lat;
-    let longitude = json.results[0].locations[0].displayLatLng.lng;
-    let issUrl = "http://api.open-notify.org/iss-pass.json?lat="
-                 + latitude + "&lon="+ longitude;
-
-    console.log(latitude);
-    console.log(longitude);
-
-    fetch(issUrl)
-    .then(response => response.json())
-    .then(json => console.log(json));
-}
-
-
 
 fetch(spacePeopleUrl)
-.then(response => response.json())
-.then(peopleHTML)
-.catch(err => console.log(err));
+  .then((response) => response.json())
+  .then(peopleHTML)
+  .catch((err) => console.log(err));
 
-btn.addEventListener("click" , (event) => {
-    event.preventDefault();
-
-    fetch(mapQuestUrl + address.value)
-    .then(response => response.json())
-    .then(latLng)
-
-});
+getMap();
