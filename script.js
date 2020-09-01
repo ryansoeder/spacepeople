4c947e14c51672ea967beeb172498bdf22bce639
Ryan Soeder <mr.soeder@gmail.com>
2020-09-01 18:13:51 -0400

    JS refactor

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
+// DOM Elements
 const peopleDiv = document.querySelector('#people');
 const mapDiv = document.querySelector('#map');
+
+// urls to fetch/build
 const spacePeopleUrl = 'http://api.open-notify.org/astros.json';
+const issUrl = 'http://api.open-notify.org/iss-now.json';
+const mapQuestUrl =
+	'https://open.mapquestapi.com/staticmap/v5/map?key=PQxsFLRrnOeYjpsUPqnkYBqjA7uGGteU&center=';
+
+
+// ------------------------------------------
+//  FETCH FUNCTIONS
+// ------------------------------------------
+
+fetch(spacePeopleUrl)
+	.then((response) => response.json())
+	.then(peopleHTML)
+	.catch((err) => console.log(err));
+
+fetch(issUrl)
+	.then((response) => response.json())
+	.then((data) => getMap(data.iss_position))
+	.catch((err) => {
+		mapDiv.innerHTML = `<h1>Sorry, there was an error fetching this information.</h1>`;
+		console.log(Error(err));
+	});
+
+// ------------------------------------------
+//  HELPER FUNCTIONS
+// ------------------------------------------
 
 function peopleHTML(json) {
 	let heading = `<h1>There are ${json.number} people in SPACE</h1>
@@ -17,32 +45,12 @@ function peopleHTML(json) {
 	peopleDiv.innerHTML = html;
 }
 
-function getMap() {
-	const issUrl = 'http://api.open-notify.org/iss-now.json';
-	const mapQuestUrl =
-		'https://open.mapquestapi.com/staticmap/v5/map?key=PQxsFLRrnOeYjpsUPqnkYBqjA7uGGteU&center=';
-
-	fetch(issUrl)
-		.then((response) => response.json())
-		.then((json) => {
-			let map =
-				mapQuestUrl +
-				json.iss_position.latitude +
-				',' +
-				json.iss_position.longitude +
-				'&size=500,400&zoom=4&banner=location+of+our+heros';
-			mapDiv.innerHTML = `<img src=${map}>`;
-		})
-		.catch((err) => {
-			mapDiv.innerHTML = `<h1>Sorry, there was an error fetching this information.</h1>`;
-			console.log(Error(err));
-		});
-	setTimeout(getMap, 10000);
+function getMap(data) {
+	let map =
+		mapQuestUrl +
+		data.latitude +
+		',' +
+		data.longitude +
+		'&size=500,400&zoom=4&banner=location+of+our+heros';
+	mapDiv.innerHTML = `<img src=${map}>`;
 }
-
-fetch(spacePeopleUrl)
-	.then((response) => response.json())
-	.then(peopleHTML)
-	.catch((err) => console.log(err));
-
-getMap();
